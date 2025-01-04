e404f2611bb7ab7688293221e693fc2591376360
Mazen Shawky <mazenshawqy@gmail.com>
2025-01-04 13:50:51 +0200

    Initialize project

---
+++ lib/app/todo_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_v2/modules/tasks/presentation/screens/tasks_screen.dart';
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ToDo App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const TasksScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app_v2/app/todo_app.dart';
+
+void main() {
+  runApp(const ToDoApp());
+}
