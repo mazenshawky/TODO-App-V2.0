478f0c73b098f6d4a78887e53ed1f5f6f4f1bd89
Mazen Shawky <mazenshawqy@gmail.com>
2025-01-04 14:19:24 +0200

    Add dependencies and font

--- lib/app/todo_app.dart
+++ lib/app/todo_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_v2/core/utils/app_fonts.dart';
 import 'package:todo_app_v2/modules/tasks/presentation/screens/tasks_screen.dart';
 
 class ToDoApp extends StatelessWidget {
@@ -10,6 +11,7 @@ class ToDoApp extends StatelessWidget {
       title: 'ToDo App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        fontFamily: FontConstants.poppinsFontFamily,
         useMaterial3: true,
       ),
       home: const TasksScreen(),
